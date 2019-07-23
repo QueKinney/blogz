db9094b5f726f041f1d6bc5d0e6a9f57571c348b
QueKinney <q_kinney@yahoo.com>
2019-07-22 20:27:47 -0500

    update code

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-from flask import Flask, request, redirect, render_template
+from flask import Flask, request, redirect, render_template, flash, session 
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:YES@localhost:3306/build-a-blog'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://blogz:blogz@localhost:3306/blogz'
 app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
 app.secret_key = 'y337kGcys&zP3B'
@@ -15,30 +15,76 @@ class Blog(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(120))
     body = db.Column(db.String (255))
-    owner_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
-    def __init__(self, title, body):
+    def __init__(self, title, body, user):
         self.title = title
         self.body = body
-        self.owner = owner
+        self.user = user
 
 class User(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True)
+    username = db.Column(db.String(120), unique=True)
     password = db.Column(db.String(120))
-    blogs = db.relationship('Blog', backref='owner')
+    blog = db.relationship('Blog', backref='user')
 
-    def __init__(self, email, password):
-        self.email = email
+    def __init__(self, username, password):
+        self.username = username
         self.password = password
 
+@app.before_request
+def require_login():
+    allowed_routes = ['login', 'signup']
+    if request.endpoint not in allowed_routes and 'username' not in session:
+        return redirect('/login')
 
-#app routes for blog, newpost
 
-#blog should render the blogs in order with a link for them to pull up individually
-#individual can be posted under here in an if else statement. 
-#if you're not displaying blog you will display the individual post
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    if request.method == 'POST':
+
+        username = request.form["username"]
+        password = request.form["password"]
+        verify_password = request.form["verify_password"]
+        user = User.query.filter_by(username=username).first()
+   
+        
+        if user and user.password == password:
+                session['username'] = username
+                flash("Logged in")
+                return redirect('/newpost')
+        else:
+            flash('User password incorrect, or user does not exist', 'error')
+
+    return render_template('login.html')
+
+
+@app.route('/signup', methods=['POST', 'GET'])
+def signup():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        verify = request.form['verify_password']
+
+        # TODO - validate user's data
+
+        existing_user = User.query.filter_by(username=username).first()
+        if not existing_user:
+            new_user = User(username, password)
+            db.session.add(new_user)
+            db.session.commit()
+            session['username'] = username
+            return redirect('/newpost')
+        else:
+            flash('User Already Exist','error')
+    return render_template('signup.html')
+
+@app.route('/logout')
+def logout():
+    del session['username']
+    return redirect('/')
+
 
 @app.route('/blog', methods=['POST', 'GET'])
 def blog():
@@ -57,71 +103,26 @@ def blog():
 
 @app.route('/newpost', methods=['POST','GET'])
 def newpost():
-    
+    user = User.query.filter_by(username=session['username']).first()    
 
     #start with this if since this will be both a GET and POST page
-    if request.method == 'POST':
-        title_error = ""
-        blog_error = ""
-
+    if request.method == 'POST':  
         title = request.form["title"]
-        blog = request.form["blog"]
-        error = 0
-
-        if len(title) < 3:
-            error +=1
-            title_error = "Please Enter a title"
-        if len(blog) < 3:
-            error += 1 
-            blog_error ="Enter a blog Post"
-        if error !=0:
-            return render_template('newpost.html',title=title,title_error=title_error,blog=blog,blog_error=blog_error)
-        else:
-            #rewrite this to fit into your posts to save it to the server
-        #make sure to make checks to see if everything is correct before saving the server
-            new_blog = Blog(title,blog)
-            db.session.add(new_blog)
-            db.session.commit()
-            return redirect ("/blog?id=" + str(new_blog.id))
-    else:  
+        body = request.form["body"]
+        new_blog = Blog(title,body,user)
+        db.session.add(new_blog)
+        db.session.commit()
+        blog = Blog.query.order_by('-Blog.id').first()
+        return render_template ("singleblog.html",blog=blog)
+    else:    
         return render_template('newpost.html')
 
-app.route("/", methods=['GET'])
-    def index():
-        return render_template("base.html") 
-    @app.route("/validate", methods = ['POST'])
-    def validate(): 
-        error = 0
-        username_error = ""
-        password_error = ""
-        verify_password_error = ""
-        email_error = ""
+@app.route("/", methods=['GET'])
+def index():
+     return render_template("base.html") 
 
-        username = request.form["username"]
-        password = request.form["password"]
-        verify_password = request.form["verify_password"]
-        email = request.form["email"]
-
-
-        if len(username) < 3 or len(username) > 20:
-            error += 1
-            username_error = "Enter Valid Username"
-        if len(password) < 3 or len(password) > 20:
-            error += 1
-            password_error = "Enter Valid Password"
-        if verify_password != password:
-            error += 1
-            verify_password_error = "Passwords does not match"
-        if email != "":
-            
-            if ("@"  not in email) or("." not in email):
-                error +=1
-                email_error = "Enter valid email"
-        if error!= 0:
-            return render_template("base.html",username=username,username_error=username_error,password_error=password_error,verify_password_error=verify_password_error, email_error=email_error)
-        else:
-            return render_template("hello.html",username=username,password=password,verify_password=verify_password,email=email)
-
+ 
+    
 
 #leave this as is
 if __name__ == '__main__':
